6186cfdd6439bec154355c4f8a95ff3905d09c62
joao pedro <j.psilva3119@gmail.com>
2022-07-10 10:34:52 -0300

    add video

--- pages/components/Banner.jsx
+++ pages/components/Banner.jsx
@@ -1,7 +1,19 @@
+import styles from "../../styles/Banner.module.css";
+
 const Banner = () => {
-    return ( 
+  return (
     <>
-    </> );
-}
- 
+      
+        <div className={styles.video}>
+          <video autoPlay loop>
+            <source src="/images/video.mp4" type="video/mp4" />
+          </video>
+          <h1>Os melhores Filmes</h1>
+        
+      </div>
+      
+    </>
+  );
+};
+
 export default Banner;
